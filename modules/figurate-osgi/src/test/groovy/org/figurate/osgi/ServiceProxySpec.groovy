f3357b29ab9000a8b25a44feb37707260dd5bd62
Ben Fortuna <benfortuna@gmail.com>
2013-09-27 01:08:41 +1000

    Added proxy test

--- modules/figurate-osgi/src/test/groovy/org/figurate/osgi/ServiceProxySpec.groovy
+++ modules/figurate-osgi/src/test/groovy/org/figurate/osgi/ServiceProxySpec.groovy
@@ -1,6 +1,7 @@
 package org.figurate.osgi
 
 import org.osgi.framework.BundleContext
+import org.osgi.framework.ServiceReference;
 
 import spock.lang.Specification
 
@@ -11,12 +12,27 @@ class ServiceProxySpec extends Specification {
 		BundleContext bundleContext = Mock()
 		
 		and: 'create service proxy'
-		List proxy = ServiceProxy.newProxyInstance(getClass().getClassLoader(), List, bundleContext)
+		List proxy = ServiceProxy.newProxyInstance(getClass().classLoader, List, bundleContext)
 		
 		when:
 		proxy.size()
 		
 		then:
 		thrown(ServiceNotAvailableException)
+	}
+	
+	def 'verify service is called when service is available'() {
+		setup: 'mock bundle context'
+		BundleContext bundleContext = Mock()
+		def reference = {'reference'} as ServiceReference<List>
+		bundleContext.getServiceReferences(List.name, null) >> reference
+		bundleContext.getService(reference) >> ['1']
+		
+		and: 'create service proxy'
+		List proxy = ServiceProxy.newProxyInstance(getClass().classLoader, List, bundleContext)
+		
+		expect:
+		proxy.size() == 1
+		proxy[0] == '1'
 	}
 }
