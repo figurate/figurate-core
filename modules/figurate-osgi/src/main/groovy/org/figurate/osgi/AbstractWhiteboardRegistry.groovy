a3e6a3d45d3f9135df270623b2a7ec9f9c803daf
Ben Fortuna <benfortuna@gmail.com>
2013-09-27 11:58:45 +1000

    Added abstract whiteboard registry to support connecting publishers and subscribers dynamically

---
+++ modules/figurate-osgi/src/main/groovy/org/figurate/osgi/AbstractWhiteboardRegistry.groovy
@@ -0,0 +1,100 @@
+package org.figurate
+
+import groovy.transform.CompileStatic
+
+import java.util.List
+import java.util.Map
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
+
+/**
+ * Base class for registry implementations to assist implementation of the
+ * whiteboard pattern.
+ * 
+ * @param <P>
+ *            the publisher type
+ * @param <S>
+ *            the subscriber type
+ */
+@CompileStatic
+abstract class AbstractWhiteboardRegistry<P, S> {
+
+    private final List<P> publishers
+
+    private final Map<S, Map<String, ?>> subscribers
+
+    AbstractWhiteboardRegistry() {
+        publishers = new CopyOnWriteArrayList<P>()
+        subscribers = new ConcurrentHashMap<S, Map<String, ?>>()
+    }
+
+    /**
+     * @param publisher the source
+     * @param properties additional publishing properties
+     */
+    final void registerPublisher(P publisher, Map<String, ?> properties) {
+        publishers.add(publisher)
+        for (S subscriber : subscribers.keySet()) {
+            subscribe(publisher, subscriber, subscribers.get(subscriber))
+        }
+    }
+
+    /**
+     * @param publisher the source
+     * @param properties additional publishing properties
+     */
+    final void unregisterPublisher(P publisher, Map<String, ?> properties) {
+        publishers.remove(publisher)
+        for (S subscriber : subscribers.keySet()) {
+            unsubscribe(publisher, subscriber, subscribers.get(subscriber))
+        }
+    }
+
+    /**
+     * @param subscriber the destination
+     * @param properties additional subscription properties
+     */
+    final void registerSubscriber(S subscriber, Map<String, ?> properties) {
+        subscribers.put(subscriber, properties);
+        for (P publisher : publishers) {
+            subscribe(publisher, subscriber, properties);
+        }
+    }
+
+    /**
+     * @param subscriber the destination
+     * @param properties additional subscription properties
+     */
+    final void unregisterSubscriber(S subscriber,
+            Map<String, ?> properties) {
+        subscribers.remove(subscriber)
+        for (P publisher : publishers) {
+            unsubscribe(publisher, subscriber, properties)
+        }
+    }
+
+    /**
+     * Unregister all subscribers.
+     */
+    final void unregisterAll() {
+        for (S subscriber : subscribers.keySet()) {
+            unregisterSubscriber(subscriber, subscribers.get(subscriber))
+        }
+    }
+    
+    /**
+     * @param publisher the source
+     * @param subscriber the destination
+     * @param properties additional subscription properties
+     */
+    protected abstract void subscribe(P publisher, S subscriber,
+            Map<String, ?> properties)
+
+    /**
+     * @param publisher the source
+     * @param subscriber the destination
+     * @param properties additional subscription properties
+     */
+    protected abstract void unsubscribe(P publisher, S subscriber,
+            Map<String, ?> properties)
+}
