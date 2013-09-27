048633c1d9749bc655cd7845f25f639e51722f1a
Ben Fortuna <benfortuna@gmail.com>
2013-09-27 11:37:39 +1000

    Added rety invocation handler for using retry handler with proxy

---
+++ modules/figurate-common/src/main/groovy/org/figurate/RetryInvocationHandler.groovy
@@ -0,0 +1,45 @@
+package org.figurate
+
+import groovy.transform.CompileStatic
+
+import java.lang.reflect.InvocationHandler
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
+
+@CompileStatic
+class RetryInvocationHandler implements InvocationHandler {
+
+    private final Object target
+    
+    private final RetryHandler retryHandler
+    
+    /**
+     * @param target the invocation target
+     * @param retryHandler controls retry on invocation exceptions
+     */
+    public RetryInvocationHandler(Object target, RetryHandler retryHandler) {
+        this.target = target
+        this.retryHandler = retryHandler
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object result = null
+        boolean retry = true
+        while (retry) {
+            try {
+                result = method.invoke(target, args)
+                break
+            }
+            catch (InvocationTargetException e) {
+                retry = retryHandler.retryOnException(e.targetException)
+                if (!retry) {
+                    throw e.targetException
+                }
+            }
+        }
+        return result
+    }
+}
