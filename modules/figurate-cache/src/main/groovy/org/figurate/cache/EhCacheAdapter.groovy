e419cec7c22a31726b99e1b66590556d1ec237aa
Ben Fortuna <benfortuna@gmail.com>
2013-10-01 17:09:25 +1000

    Created cache adapter for ehcache

---
+++ modules/figurate-cache/src/main/groovy/org/figurate/cache/EhCacheAdapter.groovy
@@ -0,0 +1,30 @@
+package org.figurate.cache
+
+import groovy.transform.CompileStatic
+
+import net.sf.ehcache.Cache
+import net.sf.ehcache.Element
+
+@CompileStatic
+class EhCacheAdapter implements CacheAdapter {
+
+    private final Cache cache
+    
+    EhCacheAdapter(Cache cache) {
+        this.cache = cache
+    }
+    
+    <T> T get(CacheEntry entry, Object...args) {
+        T value = null
+        final String key = entry.getKey(args)
+        Element cacheElement = cache.get(key)
+        if (cacheElement != null) {
+            value = cacheElement.getValue()
+        }
+        else {
+            value = entry.load(args)
+            cache.put(new Element(key, value))
+        }
+        return value
+    }
+}
