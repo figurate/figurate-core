57881ad232fc81e94699dcc535adc282670a113e
Ben Fortuna <fortuna@micronode.com>
2013-10-02 23:44:13 +1000

    Added integration test

---
+++ src/test/groovy/org/figurate/test/BootstrapTest.groovy
@@ -0,0 +1,30 @@
+package org.figurate.test
+
+import static org.ops4j.pax.exam.CoreOptions.*
+
+import org.junit.Test;
+import org.junit.runner.RunWith
+import org.ops4j.pax.exam.Configuration
+import org.ops4j.pax.exam.Option
+import org.ops4j.pax.exam.junit.PaxExam
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy
+import org.ops4j.pax.exam.spi.reactors.PerMethod
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
+class BootstrapTest {
+
+	@Configuration
+	public Option[] config() {
+		[
+			mavenBundle('org.codehaus.groovy', 'groovy-all', '2.1.7'),
+			bundle('file:/home/fortuna/Development/figurate-core/modules/figurate-common/build/libs/figurate-common.jar'),
+			junitBundles()
+		] as Option[]
+	}
+	
+	@Test
+	void test() {
+		println 'works'
+	}
+}
