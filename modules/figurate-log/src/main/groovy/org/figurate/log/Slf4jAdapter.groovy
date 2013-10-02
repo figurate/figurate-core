f8a326e14fd9f4cd6d237dcc23795d0eaf8bbc84
Ben Fortuna <benfortuna@gmail.com>
2013-10-02 13:31:17 +1000

    Create Slf4jAdapter.groovy

---
+++ modules/figurate-log/src/main/groovy/org/figurate/log/Slf4jAdapter.groovy
@@ -0,0 +1,75 @@
+package org.figurate.log
+
+import groovy.transform.CompileStatic
+
+import org.slf4j.Logger
+
+@CompileStatic
+public class Slf4jAdapter extends AbstractLogAdapter {
+
+    private final Logger logger
+    
+    public Slf4jAdapter(Logger logger) {
+        this.logger = logger
+    }
+
+    @Override
+    protected boolean isDebugEnabled() {
+        logger.debugEnabled
+    }
+
+    @Override
+    protected boolean isInfoEnabled() {
+        logger.infoEnabled
+    }
+
+    @Override
+    protected boolean isWarnEnabled() {
+        logger.warnEnabled
+    }
+
+    @Override
+    protected boolean isErrorEnabled() {
+        logger.errorEnabled
+    }
+
+    @Override
+    protected void debug(String message) {
+        logger.debug(message)
+    }
+    
+    @Override
+    protected void debug(String message, Throwable e) {
+        logger.debug(message, e)
+    }
+    
+    @Override
+    protected void info(String message) {
+        logger.info(message)
+    }
+    
+    @Override
+    protected void info(String message, Throwable e) {
+        logger.info(message, e)
+    }
+    
+    @Override
+    protected void warn(String message) {
+        logger.warn(message)
+    }
+    
+    @Override
+    protected void warn(String message, Throwable e) {
+        logger.warn(message, e)
+    }
+    
+    @Override
+    protected void error(String message) {
+        logger.error(message)
+    }
+    
+    @Override
+    protected void error(String message, Throwable e) {
+        logger.error(message, e)
+    }
+}
