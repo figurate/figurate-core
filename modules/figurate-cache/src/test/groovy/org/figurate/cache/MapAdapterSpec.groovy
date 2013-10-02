51d79ef671b1d9615ec7dc384f1d01b0697949f2
Ben Fortuna <benfortuna@gmail.com>
2013-10-02 11:38:18 +1000

    Added infinispan test

--- modules/figurate-cache/src/test/groovy/org/figurate/cache/MapAdapterSpec.groovy
+++ modules/figurate-cache/src/test/groovy/org/figurate/cache/MapAdapterSpec.groovy
@@ -2,11 +2,27 @@ package org.figurate.cache
 
 import spock.lang.Specification
 
+import org.infinispan.manager.DefaultCacheManager
+import org.infinispan.Cache
+
 class MapAdapterSpec extends Specification {
 
 	def 'verify cache get'() {
 		setup:
 		MapAdapter cache = [[:]]
+		
+		when:
+		CacheEntryImpl entry = []
+		
+		then: 'subsequent retrievals should return the cached value'
+		def value = cache.get(entry, 2)
+		assert cache.get(entry, 2) == value
+	}
+
+	def 'verify infinispan cache get'() {
+		setup:
+		DefaultCacheManager cacheManager = []
+		Cache cache = cacheManager.cache
 		
 		when:
 		CacheEntryImpl entry = []
