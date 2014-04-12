f4cba67169986aadf7a091fb0101ae855ed4a0ba
Ben Fortuna <bfortuna@odecee.com.au>
2014-04-12 12:29:29 +1000

    Async backlog

--- modules/figurate-log/src/main/groovy/org/figurate/log/osgi/LogServiceAdapter.groovy
+++ modules/figurate-log/src/main/groovy/org/figurate/log/osgi/LogServiceAdapter.groovy
@@ -2,9 +2,13 @@ package org.figurate.log.osgi
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
+import groovyx.gpars.GParsExecutorsPool
 import org.apache.felix.scr.annotations.*
-import org.figurate.log.*
-import org.fusesource.jansi.Ansi
+import org.figurate.log.AnsiLogEntry
+import org.figurate.log.AsyncLogAdapter
+import org.figurate.log.LogAdapter
+import org.figurate.log.LogEntry.Level
+import org.figurate.log.Slf4jLogAdapter
 import org.fusesource.jansi.AnsiConsole
 import org.osgi.framework.Bundle
 import org.osgi.service.log.LogEntry
@@ -12,13 +16,9 @@ import org.osgi.service.log.LogListener
 import org.osgi.service.log.LogReaderService
 import org.osgi.service.log.LogService
 import org.slf4j.LoggerFactory
-import org.figurate.log.LogEntry.Level
 
 import static org.figurate.log.LogEntry.Level.*
-import static org.fusesource.jansi.Ansi.Color.CYAN
-import static org.fusesource.jansi.Ansi.Color.GREEN
-import static org.fusesource.jansi.Ansi.Color.RED
-import static org.fusesource.jansi.Ansi.Color.YELLOW
+import static org.fusesource.jansi.Ansi.Color.*
 
 /**
  * Created by fortuna on 9/02/14.
@@ -31,26 +31,33 @@ class LogServiceAdapter implements LogListener {
 
     private Map<Long, LogAdapter> logAdapters = [:]
 
-    private org.figurate.log.LogEntry debug = new AnsiLogEntry(level: Debug, template: '%s %s %s %s', colour: CYAN)
+//    private org.figurate.log.LogEntry debug = new AnsiLogEntry(level: Debug, template: '%s %s %s %s', colour: CYAN)
+    private org.figurate.log.LogEntry debug = new BundleLogEntry(level: Debug)
     private org.figurate.log.LogEntry info = new BundleLogEntry(level: Info)
-    private org.figurate.log.LogEntry warning = new AnsiLogEntry(level: Warn, template: '%s %s %s %s', colour: YELLOW)
-    private org.figurate.log.LogEntry error = new AnsiLogEntry(level: Level.Error, template: '%s %s %s %s', colour: RED)
+    private org.figurate.log.LogEntry warning = new BundleLogEntry(level: Warn)
+    private org.figurate.log.LogEntry error = new BundleLogEntry(level: Level.Error)
+//    private org.figurate.log.LogEntry warning = new AnsiLogEntry(level: Warn, template: '%s %s %s %s', colour: YELLOW)
+//    private org.figurate.log.LogEntry error = new AnsiLogEntry(level: Level.Error, template: '%s %s %s %s', colour: RED)
 
     @Activate
     void activate() {
-        AnsiConsole.systemInstall()
+//        AnsiConsole.systemInstall()
     }
 
     @Deactivate
     void deactivate() {
-        AnsiConsole.systemUninstall()
+//        AnsiConsole.systemUninstall()
     }
 
     void bind(LogReaderService logReaderService, Map<String, ?> properties) {
         logReaderService.addLogListener(this)
         log.debug 'Writing log backlog..'
-        logReaderService.log.toList().reverseEach {
-            logged((LogEntry) it)
+        GParsExecutorsPool.withPool {
+            GParsExecutorsPool.executeAsync {
+                logReaderService.log.toList().reverseEach {
+                    logged((LogEntry) it)
+                }
+            }
         }
         log.debug 'Writing log backlog complete.'
     }
