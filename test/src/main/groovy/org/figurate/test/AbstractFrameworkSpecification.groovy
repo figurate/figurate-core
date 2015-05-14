c0a76623e21ae7e35d95f8bc7639743ae9ee6eb5
Ben Fortuna <bfortuna@odecee.com.au>
2015-05-15 00:13:35 +1000

    Wait for framework to start

--- test/src/main/groovy/org/figurate/test/AbstractFrameworkSpecification.groovy
+++ test/src/main/groovy/org/figurate/test/AbstractFrameworkSpecification.groovy
@@ -1,13 +1,20 @@
 package org.figurate.test
 
+import groovy.util.logging.Slf4j
 import org.figurate.FrameworkLauncher
+import org.osgi.framework.FrameworkEvent
+import org.osgi.framework.FrameworkListener
 import org.osgi.framework.launch.Framework
 import spock.lang.Shared
 import spock.lang.Specification
 
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
 /**
  * Created by fortuna on 11/04/14.
  */
+@Slf4j
 abstract class AbstractFrameworkSpecification extends Specification {
 
     static {
@@ -23,11 +30,16 @@ abstract class AbstractFrameworkSpecification extends Specification {
                         return false;
                     }
                 });
+
+        System.setProperty('logback.configurationFile', 'build/config/logback.groovy')
     }
 
     @Shared def config
     @Shared Framework osgi
     @Shared def binding = [:]
+
+    @Shared
+    CountDownLatch frameworkStarted = new CountDownLatch(1)
 
     void loadConfig(def configFile) {
         Binding binding = []
@@ -40,6 +52,21 @@ abstract class AbstractFrameworkSpecification extends Specification {
         FrameworkLauncher launcher = [config: config]
         launcher.launch(binding)
         osgi = launcher.osgi
+        binding.bundleContext.addFrameworkListener({ e ->
+            if (e.type == FrameworkEvent.STARTED) {
+                log.info "### Framework started. ###"
+                frameworkStarted.countDown()
+            } else if (e.type == FrameworkEvent.ERROR) {
+                log.error '### Error starting framework ###', e.throwable
+            } else {
+                log.info "### $e.type, $e.bundle: $e.throwable ###"
+            }
+        } as FrameworkListener)
+    }
+
+    def setup() {
+        // wait for framework to start fully before running tests.
+        frameworkStarted.await(30, TimeUnit.SECONDS)
     }
 
     abstract getConfigLocation();
